dc5c2c9dc36bd45ff41ff90a040d2597fa9f2014
Evan Hensleigh <futuraprime@gmail.com>
2015-04-24 19:42:48 -0400

    And now we have seat positioning.

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ function getTransformString(x, y, css) {
 
 var chart = d3.select('#interactive').append('svg')
   .attr('id', 'chart')
-  .attr('width', 600)
-  .attr('height', 600);
+  .attr('width', width)
+  .attr('height', height);
 
 // lab, con, ld, ukip, green, snp, plaid, other
 var parties = {
@@ -119,8 +119,9 @@ var arc = d3.svg.arc()
 var arcContainer = chart.append('g')
   .attr('transform', getTransformString(width/2, height/2));
 
+var partyArcs = arcLayout(results);
 var arcJoin = arcContainer.selectAll('.seat-arc')
-  .data(arcLayout(results));
+  .data(partyArcs);
 arcJoin.enter().append('svg:path')
   .classed('seat-arc', true)
   .attr('fill', function(d) {
@@ -149,3 +150,83 @@ var nestedSeatArray = nester.entries(seatArray);
 // I think the approach here is to come up with a sort of polar grid-ish
 // thing and then assign them each places based on the index in the list
 // so we have a width per theta and we base things around that. maybe?
+var seatGroup = chart.append('svg:g')
+  .classed('seats-group', true);
+
+var partyJoin = seatGroup.selectAll('.seats-party')
+  .data(nestedSeatArray);
+
+partyJoin.enter().append('svg:g')
+  .classed('seats-party', true);
+
+var seatSize = 10;
+var seatSpace = 12;
+
+var ringRadii = _.range(50, 350, seatSpace);
+var ringSteps = _.map(ringRadii, function(v, idx) {
+  return seatSpace / v;
+});
+
+function radToDeg(rad) { return 180 * rad / Math.PI; }
+
+partyJoin.each(function(d) {
+  var el = d3.select(this);
+  // move this to the center...
+  el.attr('transform', getTransformString(width/2, height/2));
+
+  var seatJoin = el.selectAll('.seat')
+    .data(d.values);
+
+  var dataArc = _.find(partyArcs, function(pArc) {
+    return pArc.data.key === d.key;
+  });
+  
+  // so now we have start and end angles, now we've got to figure out
+  // how many seats per arc
+  // the length of the arc is r * angle width
+  // console.log(50 * (dataArc.startAngle - dataArc.endAngle), d.key);
+  var arcSeats = _.map(ringRadii, function(radius) {
+    var len = radius * (dataArc.startAngle - dataArc.endAngle);
+    return Math.ceil(len / seatSpace);
+  });
+  var arcSeatTotals = _.map(arcSeats, function(v, idx) {
+    return _.sum(arcSeats.slice(0, idx + 1));
+  });
+  // console.log(arcSeats, arcSeatTotals, d.key);
+
+  seatJoin.enter()
+    .append('svg:rect')
+    .classed('seat', true)
+    .attr({
+      x : -seatSize/2, y: -seatSize/2, width: seatSize, height : seatSize,
+      fill : function(d) {
+        return parties[d.key].color;
+      }
+    })
+    .transition().duration(200).delay(function(d, i) {
+      return i * 10;
+    })
+    // .attr('transform', function(d, idx) {
+    //   var transformString = '';
+    //   var rank = _.findIndex(arcSeatTotals, function(v) { return v > idx; });
+    //   var position = idx - (arcSeatTotals[rank - 1] || 0);
+    //   var angle = dataArc.endAngle;
+    //   angle += position * ringSteps[rank];
+    //   transformString += 'translate(0,'+(50 + rank * seatSpace)+')';
+    //   transformString += 'rotate('+(180 + radToDeg(angle))+')';
+    //   return transformString;
+    // })
+    // .transition().duration(200)
+    .attr('transform', function(d, idx) {
+      var transformString = '';
+      var rank = _.findIndex(arcSeatTotals, function(v) { return v > idx; });
+      var position = idx - (arcSeatTotals[rank - 1] || 0);
+      var angle = dataArc.endAngle;
+      angle += position * ringSteps[rank];
+      transformString += 'rotate('+(180 + radToDeg(angle))+')';
+      transformString += 'translate(0,'+(50 + rank * seatSpace)+')';
+      return transformString;
+    });
+});
+
+var innerRadius = 75;
